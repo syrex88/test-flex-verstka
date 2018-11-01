d4153998f35c237bd64d11cb6cea391747de185f
Sergey Lebedev <syrex88@gmail.com>
2018-11-01 15:37:35 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="container">
+    <div class="list-items">
+      <div class="item">
+        <div>Item 1</div>
+      </div>
+      <div class="item">
+        <div>Item 2</div>
+      </div>
+      <div class="item">
+        <div>Item 3</div>
+      </div>
+      <div class="item">
+        <div>Item 3</div>
+      </div>
+    </div>
+    <div class="item gray">
+      <div>Item</div>
+    </div>
+  </section>
+</template>
+
+<style>
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+}
+
+.list-items {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.item {
+  height: 25vh;
+  width: 25vh;
+  padding: 5px;
+}
+
+.item div {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.item.gray div {
+  background-color: gray;
+}
+
+.list-items .item div {
+  background-color: red;
+}
+
+@media (min-width: 525px) {
+  .item {
+    height: 50vh;
+    width: 50vh;
+  }
+
+  .list-items {
+    flex-direction: row;
+    flex-wrap: wrap;
+    max-width: 100vh;
+  }
+}
+
+</style>
